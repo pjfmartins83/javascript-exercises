2b46079f659d6395138c9cc4943dacb1fc808ec2
pjfmartins83 <pjfmartins83@gmail.com>
2024-12-02 16:08:32 -0300

    added factorial function and tests

---
+++ factorial/factorial.js
@@ -0,0 +1,13 @@
+// factorial.js
+function factorial(n) {
+    if (n < 0) {
+        return 'Input must be a non-negative integer';
+    }
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+module.exports = factorial;
