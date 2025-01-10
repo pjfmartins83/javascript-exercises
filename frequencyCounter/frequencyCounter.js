e3c8db85ba9c441b944be3f3af8db9d0f81179c9
pjfmartins83 <pjfmartins83@gmail.com>
2025-01-10 10:45:22 -0300

    2nd run

--- frequencyCounter/frequencyCounter.js
+++ frequencyCounter/frequencyCounter.js
@@ -6,16 +6,19 @@ Count the frequency of each letter that appearance in a string.
 
 **/
 
-function frequencyCounter(string) {
-    const letters = {}
-    for (const letter of string) {
+function countLetters(string) {
+    let letters = {}
+    string = string.toLowerCase();
+
+    for (let letter of string) {
         if (letters[letter]) {
-            letters[letter] += 1;
+            (letters[letter]) += 1;
         } else {
-            letters[letter] = 1;
+            (letters[letter]) = 1;
         }
     }
+
     return letters;
 }
 
-module.exports = frequencyCounter;
+module.exports = countLetters;
--- frequencyCounter/frequencyCounter.test.js
+++ frequencyCounter/frequencyCounter.test.js
@@ -1,13 +1,13 @@
-const frequencyCounter = require('./frequencyCounter');
+const countLetters = require('./frequencyCounter');
 
-test('An empty string', () => {
-    expect(frequencyCounter('')).toEqual({});
-});
+test('Empty String', () => {
+    expect(countLetters('')).toStrictEqual({});
+})
 
-test('A single character', () => {  
-    expect(frequencyCounter('a')).toEqual({a :1});
-});
+test('One letter', () => {
+    expect(countLetters('a')).toStrictEqual({a: 1});
+})
 
-test('Mixed Characters', () => {    
-    expect(frequencyCounter('abbccddd')).toEqual({a: 1, b: 2, c: 2, d: 3});
-});
+test('Two letters - 1 upper and 1 lower', () => {
+    expect(countLetters('Aa')).toStrictEqual({a: 2});
+})
