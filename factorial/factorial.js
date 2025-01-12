5dfe567a0308a23c51b9ac30216986f6575b5fe5
pjfmartins83 <pjfmartins83@gmail.com>
2025-01-12 19:29:55 -0300

    2nd run

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,12 +1,41 @@
-function factorial(n) {
-    if (n < 0) {
-        return 'Input must be a non-negative integer';
-    }
-    let result = 1;
-    for (let i = 1; i <= n; i++) {
-        result *= i;
-    }
-    return result;
-}
+/*
 
-module.exports = factorial;
+Tha Challenge: Factorial Calculation
+
+Write a JavaScript function factorial(n) that calculates the factorial of a given positive integer n.
+
+The factorial of a number n is the product of all positive integers less than or equal to n.
+It is denoted as n!. For example:
+
+5! = 5 * 4 * 3 * 2 * 1 = 120
+3! = 3 * 2 * 1 = 6
+1! = 1
+0! = 1 (by definition)
+
+Requirements:
+
+Your function should accept a single argument n, which is a non-negative integer.
+The function should return the factorial of n.
+If the input is 0, the function should return 1.
+
+*/
+
+function getFactorial(n) {
+    if (n < 0) {
+        return 'Input must be a non-negative number';
+    } else if ( n === 0) {
+        return 1;
+    } else if (n > 0) {
+        let result = 1;
+
+        for (let i = 1; i <= n; i++) {
+            result = result * i;
+        }
+
+        return result;
+    }
+}
+    
+console.log(getFactorial(10));
+
+module.exports = getFactorial;
--- factorial/factorial.test.js
+++ factorial/factorial.test.js
@@ -1,21 +1,21 @@
-const factorial = require('./factorial');
+const getFactorial = require('./factorial');
 
-test('factorial of 0 should be 1', () => {
-    expect(factorial(0)).toBe(1);
+test('Factorial of 0 should be 1', () => {
+    expect(getFactorial(0)).toBe(1);
 });
 
-test('factorial of 1 should be 1', () => {
-    expect(factorial(1)).toBe(1);
+test('Factorial of 1 should be 1', () => {
+    expect(getFactorial(1)).toBe(1);
 });
 
-test('factorial of 5 should be 120', () => {
-    expect(factorial(5)).toBe(120);
+test('Factorial of 5 should be 120', () => {
+    expect(getFactorial(5)).toBe(120);
 });
 
-test('factorial of 10 should be 3628800', () => {
-    expect(factorial(10)).toBe(3628800);
+test('Factorial of 10 should be 3628800', () => {
+    expect(getFactorial(10)).toBe(3628800);
 });
 
-test('factorial of a negative number should return an error message', () => {
-    expect(factorial(-1)).toBe('Input must be a non-negative integer');
+test('Factorial of a negative number should return an error message', () => {
+    expect(getFactorial(-1)).toBe('Input must be a non-negative number');
 });
