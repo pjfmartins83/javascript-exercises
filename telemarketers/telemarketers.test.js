db4f03a17ab7633a3cbe45598b1643162987c194
pjfmartins83 <pjfmartins83@gmail.com>
2025-02-03 10:44:18 -0300

    2nd run revisited

--- telemarketers/telemarketers.test.js
+++ telemarketers/telemarketers.test.js
@@ -1,26 +1,26 @@
 const isTelemarketer = require('./telemarketers');
 
 test('Ignore the call', () => {
-    const telephoneNumer = [8, 1, 1, 9];
-    expect(isTelemarketer(telephoneNumer)).toBe('Ignore');
+    const telephoneNumber = [8, 1, 1, 9];
+    expect(isTelemarketer(telephoneNumber)).toBe('Ignore');
 })
 
-test('Ignore the call', () => {
-    const telephoneNumer = [8, 1, 5, 4];
-    expect(isTelemarketer(telephoneNumer)).toBe('Answer');
+test('Answer the call', () => {
+    const telephoneNumber = [8, 1, 5, 4];
+    expect(isTelemarketer(telephoneNumber)).toBe('Answer');
 })
 
-test('Ignore the call', () => {
-    const telephoneNumer = [5, 2, 2, 7];
-    expect(isTelemarketer(telephoneNumer)).toBe('Answer');
+test('Answer the call', () => {
+    const telephoneNumber = [5, 2, 2, 7];
+    expect(isTelemarketer(telephoneNumber)).toBe('Answer');
 })
 
-test('Ignore the call', () => {
-    const telephoneNumer = [5, 8, 2, 8];
-    expect(isTelemarketer(telephoneNumer)).toBe('Answer');
+test('Answer the call', () => {
+    const telephoneNumber = [5, 8, 2, 8];
+    expect(isTelemarketer(telephoneNumber)).toBe('Answer');
 })
 
-test('Ignore the call', () => {
-    const telephoneNumer = [5, 8, 2, 4];
-    expect(isTelemarketer(telephoneNumer)).toBe('Answer');
+test('Answer the call', () => {
+    const telephoneNumber = [5, 8, 2, 4];
+    expect(isTelemarketer(telephoneNumber)).toBe('Answer');
 })
